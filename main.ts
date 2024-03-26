3aaf189882c9579989b972f6405311c1f103416e
Nisreen <164015373+Nisreen5@users.noreply.github.com>
2024-03-26 06:27:41 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+// BMI Calculator in Typescript
+
+let weightInKg = 70; 
+let heightInMeters = 1.75;
+let bmi = weightInKg / (heightInMeters * heightInMeters)
+console.log(`Nisreen Your BMI is ${bmi}`);
